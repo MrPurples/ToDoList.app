77c0eef424865b5032f87a6dfbd8928c1fd7864d
MrPurple <96318335+MrPurples@users.noreply.github.com>
2022-06-03 06:45:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const url = 'https://61d40a5c8df81200178a8a16.mockapi.io/api/getTodoList'
-
+const modal = document.getElementById('modal');
+const deleteButton = document.getElementById("delete-confirm");
 const getAllTasks = () => {
     fetch(url)
     .then(response => {
@@ -11,24 +12,36 @@ const getAllTasks = () => {
             data += `
                 <div class="task">
                     <div class="content">
-                        <input type="text", class="text", value='${value.taskName}'/>
+                        <input type="text", id="yes${value.id}" class="text", value='${value.taskName}', readonly="readonly"/>
                     </div>
                     <div class="actions">
-                        <button class="edit">Edit</button>
-                        <button onclick='deleteTask(${value.id})' class="delete">Delete</button>
+                        <button id="save${value.id}" onclick="saveNewTask(${value.id})" class="Save">Save</button>
+                        <button id="edit${value.id}" onclick='editTasks("${value.id}")' class="edit">Edit</button>
+                        <button onclick='deleteModal("${value.id}", "${value.taskName}")' class="delete">Delete</button>
+                    </div>
+                    <div class="date">
+                        ${value.date}
                     </div>
                 </div>
-            `
+            `;
         })
+        const task = document.getElementsByClassName("task")
         document.getElementById('tasks').innerHTML = data;
+        document.getElementById('new-todo-input').value =''
+        
     })
+    
 };
 
 const addTask = async () => {
     const newTaskName = document.getElementById("new-todo-input").value;
+    const date = new Date();
+    const formatedDate = date.getDay()+"-"+ date.getMonth()+"-"+ date.getFullYear();
     const data = {
         "taskName": newTaskName,
-        "completed": false
+        "completed": false,
+        "date": formatedDate
+
     };
     await fetch(url, {
         method: 'POST',
@@ -37,6 +50,7 @@ const addTask = async () => {
         },
         body: JSON.stringify(data)
     })
+    
     getAllTasks();
 };
 
@@ -48,11 +62,57 @@ const deleteTask = async (taskId) => {
     getAllTasks();
 }
 
-// TODO 
-/* 
-* Confirmation modal on delete
-* Input field validation (No special chars allowed!!!)
-* The New task should be the first one in the list
-* Add edit functionality
-* Create completed task design and functionality
-*/
+const editTask = async (taskId,newTaskName) => {
+    const editUrl = `${url}/${taskId}`
+    await fetch(editUrl, {
+        method:'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            taskName: newTaskName
+        })
+    });
+    getAllTasks();
+};
+
+
+
+
+var buttonID = {};
+function deleteModal(id, taskName) {
+    document.getElementById('modal').style.display = 'block'
+    buttonID.id=id;
+    buttonID.task=taskName;
+    document.getElementById('taskName').innerText=buttonID.task
+};
+
+function confirmDelete() {
+    document.getElementById('modal').style.display = 'none'
+    deleteTask(buttonID.id)
+    
+    
+}
+
+
+function cancelConfirm() {
+    document.getElementById('modal').style.display = 'none'
+}
+
+function editTasks(id) {
+document.getElementById('save' + id).style.display="block"
+document.getElementById('edit'+ id).style.display="none"
+let taskmunicipal = document.getElementById('yes'+id)
+        taskmunicipal.removeAttribute("readonly");
+        taskmunicipal.focus();
+}
+
+function saveNewTask(id) {
+    let taskmunicipal = document.getElementById('yes'+id);
+    taskmunicipal.setAttribute("readonly","");
+    document.getElementById('save' + id).style.display="none"
+    document.getElementById('edit'+ id).style.display="block"
+    let newTaskName = taskmunicipal.value;
+    editTask(id,newTaskName)
+    getAllTasks()
+}
